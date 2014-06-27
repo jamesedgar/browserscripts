6ae13ed394ca08b62be766f83a41e8ea16627a7f
Rummler <k.rummler@gmail.com>
2014-06-27 12:07:41 +0200

    Update to JQuery

--- mod_cluster.js
+++ mod_cluster.js
@@ -1,43 +1,33 @@
 // ==UserScript==
-// @name       My Fancy New Userscript
-// @namespace  http://use.i.E.your.homepage/
-// @version    0.1
-// @description  enter something useful
-// @match      https://rest.digdag.nl/mod_cluster-manager
-// @copyright  2012+, You
+// @name       Mod_Cluster Basic Styling
+// @namespace  nl.topicus
+// @version    0.11
+// @description  Some basic html and css styling for mod_cluster
+// @match      https://*.digdag.nl/mod_cluster-manager
+// @copyright  2014+, Kevin Rummler
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
 
-function addJQuery(callback) {
-    var script = document.createElement("script");
-    script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
-    script.addEventListener('load', function() {
-        var script = document.createElement("script");
-        script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-        document.body.appendChild(script);
-    }, false);
-    document.body.appendChild(script);
-}
-
-function main() {
-    jQ('body').contents().filter(function() {
+function main(){
+    $('body').contents().filter(function() {
         return this.nodeType == 3;
     }).wrap('<span></span>')
     
-    jQ("br").remove();
-    jQ('div:empty').remove();
-    jQ("span:contains('Aliases')").addClass('aliases');
-    jQ("span:contains('LBGroup')").addClass('lbgroup').wrap('<h3></h3>');
-    jQ("span:contains('Node')").addClass('node').wrap('<h4></h4>');
+    $("br").remove();
+    $('div:empty').remove();
+    $("span:contains('Aliases')").addClass('aliases');
+    $("span:contains('LBGroup')").addClass('lbgroup').wrap('<h3></h3>');
+    $("span:contains('Node')").addClass('node').wrap('<h4></h4>');
     
-    jQ("pre:contains('ENABLED')").addClass('enabled');
-    jQ("pre:contains('DISABLED')").addClass('disabled');
+    $("pre:contains('ENABLED')").addClass('enabled');
+    $("pre:contains('DISABLED')").addClass('disabled');
     
-    jQ("h4 span:contains('NOTOK')").addClass('notok');
+    $("h4 span:contains('NOTOK')").addClass('notok');
 }
 
 function addGlobalStyle(css) {
     var head, style;
-    head = document.getElementsByTagName('head')[0];
+    head = $('head')[0];
     if (!head) { return; }
     style = document.createElement('style');
     style.type = 'text/css';
@@ -53,6 +43,4 @@ addGlobalStyle('span.aliases{ margin-left:60px;}');
 addGlobalStyle('pre.disabled{ color:red;}');
 addGlobalStyle("body{ font: 14px 'Helvetica Neue', Arial, sans-serif; color: #1C1C1C; background-color: #F2F2F2;}");
 
-// load jQuery and execute the main function
-addJQuery(main);
-
+main();
